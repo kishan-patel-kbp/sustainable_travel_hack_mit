0ec6f4a8014df04855c47eb346445181ed01f83e
Kishan Patel <patel.kishan.kbp@gmail.com>
2022-10-01 16:37:28 -0400

    getting emissions for flight

--- flightapi.py
+++ flightapi.py
@@ -18,6 +18,7 @@ response = requests.get(api_url)
 
 # print(response.text[0])
 data = response.json().get("data")
+# print(data)
 pricing_options = data["results"][1].get("pricing_options")
 legs = data["results"][1].get("legs")
 
@@ -25,13 +26,40 @@ print("Origin", legs[0].get("origin"))
 print("Destination", legs[0].get("destination"))
 
 
+climate_url = 'https://beta3.api.climatiq.io/travel/flights'
+climate_headers = {
+    'content-type': 'application/json',
+    'Authorization' : 'Bearer CKA1H24HQ5M1JBGK2EXP8C3EV865'}
+climate_data = {
+        "legs": [
+            {
+                "from": "BER",
+                "to": "HAM",
+                "passengers": 2,
+                "class": "first"
+            },
+            {
+                "from": "HAM",
+                "to": "JFK",
+                "passengers": 2,
+                "class": "economy"
+            }
+        ]
+    }
 
-for idx in range(len(pricing_options) - 2):
-    print(pricing_options[idx].get("price"))
-    # print("Duration of Flight", legs[idx].get("durationInMinutes"))
+response = requests.post(url=climate_url, headers=climate_headers, json=climate_data)
+print(response.json().get("co2e"))
+
+# for idx in range(len(pricing_options) - 2):
+#     print(pricing_options[idx].get("price"))
+#     # print("Duration of Flight", legs[idx].get("durationInMinutes"))
 
 # print(f'Total users: {response.json().get("data")}')
 
 
 # response = requests.get("https://api.open-notify.org/astros.json")
 # print(response.status_code)
+
+# requests.post('http://httpbin.org/post', json={"key": "value"})
+
+# CKA1H24HQ5M1JBGK2EXP8C3EV865
